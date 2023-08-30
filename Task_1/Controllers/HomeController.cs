022acf24e36df429daa1ebc1bb040f7dc8102461
Artem Priadchenko <yorgan.info@gmail.com>
2023-08-30 08:37:23 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Task_1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public static int Users { get; set; }
+
+
+
+        public IActionResult Start()
+        {
+            Users++;
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(Users);
+        }
+
+    }
+}
